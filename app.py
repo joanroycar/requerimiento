1c36a98dfb48a4367fd170470599004c71fbd563
JOAN CASA SJL <joanroycar@gmail.com>
2023-05-21 14:53:00 -0500

    app

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
-print('Hola')
+import pickle
+with open('forecast_model.pckl', 'rb') as fin:
+    m2 = pickle.load(fin)
+
+from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
+app = Flask(__name__)
+CORS(app)
+@app.route("/katana-ml/api/v1.0/forecast/ironsteel", methods=['POST'])
+def predict():
+    horizon = int(request.json['horizon'])
+    
+    future2 = m2.make_future_dataframe(periods=horizon, freq='M')
+    forecast2 = m2.predict(future2)
+    
+    data = forecast2[['ds', 'yhat', 'yhat_lower', 'yhat_upper']][-horizon:]
+    
+    ret = data.to_json(orient='records', date_format='iso')
+    
+    return ret
+# running REST interface, port=3000 for direct test
+if __name__ == "__main__":
+    app.run()
